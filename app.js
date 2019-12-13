62c948370d227d287d85ec5df91af1d206333179
Tim <tim.weinert@gmail.com>
2019-12-13 11:54:05 +1100

    JS particles included

---
+++ app.js
@@ -0,0 +1,18 @@
+window.
+    onload
+    = function () {
+        Particles.
+            init
+            ({
+                selector:
+                    '.background',
+                    maxParticles: 
+                    200
+                    ,
+                            color: 
+                    '#48F2E3'
+                    ,
+                            connectParticles: 
+                    true
+            });
+    };
