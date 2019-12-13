5eb5358c6ee657631b8419a4fdb9637057f8f087
Tim <tim.weinert@gmail.com>
2019-12-13 12:44:36 +1100

    Background image add + resized

--- app.js
+++ app.js
@@ -6,13 +6,68 @@ window.
             ({
                 selector:
                     '.background',
-                    maxParticles: 
+                maxParticles:
                     200
-                    ,
-                            color: 
+                ,
+                color:
                     '#48F2E3'
-                    ,
-                            connectParticles: 
+                ,
+                connectParticles:
                     true
             });
     };
+
+
+//RESPONSIVE DESIGN
+Particles.
+    init
+    ({
+
+        // normal options
+        selector:
+            '.background'
+        ,
+        maxParticles:
+            450
+        ,
+
+        // options for breakpoints
+        responsive: [
+            {
+                breakpoint:
+                    768
+                ,
+                options: {
+                    maxParticles:
+                        200
+                    ,
+                    color:
+                        '#48F2E3'
+                    ,
+                    connectParticles:
+                        false
+                }
+            }, {
+                breakpoint:
+                    425
+                ,
+                options: {
+                    maxParticles:
+                        100
+                    ,
+                    connectParticles:
+                        true
+                }
+            }, {
+                breakpoint:
+                    320
+                ,
+                options: {
+                    maxParticles:
+                        0
+
+                    // disables particles.js
+                }
+            }
+        ]
+    });
